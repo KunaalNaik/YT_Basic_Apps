243930fead1dc399281c76c39197a5ec3518671e
Kunaal <kunaal@users.noreply.github.com>
2020-08-28 06:38:20 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -2,7 +2,18 @@ import streamlit as st
 
 st.title("Basic App")
 
-num_feet = st.number_input("Your Height (Feet)")
-num_inches = st.number_input("Your Height (Inches)")
+st.sidebar.info("Choose Metric")
+sel_metric = st.sidebar.selectbox(
+    "Choose your metric",
+    ("Kgs and Meters", "Pounds and Inches"))
 
-num_weight = st.number_input("Your Weight (Pounds)")
+if sel_metric == "Kgs and Meters":
+    num_height = st.number_input("Your Height (meters)", 1.65)
+    num_weight = st.number_input("Your Weight (Kgs)", 68)
+    BMI = num_weight / (num_height ** 2)
+else:
+    num_height = st.number_input("Your Height (Inches)", 65)
+    num_weight = st.number_input("Your Weight (Pounds)", 150)
+    BMI = (num_weight / (num_height ** 2)) * 703
+
+st.success('The output is {}'.format(BMI))
